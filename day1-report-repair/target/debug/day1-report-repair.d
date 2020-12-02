70734b01952e13850dbdbb8f9d159a64d88c9d35
rafaavc <rafavc2508@gmail.com>
2020-12-02 10:43:30 +0000

    Removing useless stuff

--- day1-report-repair/target/debug/day1-report-repair.d
+++
@@ -1,1 +0,0 @@
-/home/rafaavc/Documents/projects/advent-of-code-2020/day1-report-repair/target/debug/day1-report-repair: /home/rafaavc/Documents/projects/advent-of-code-2020/day1-report-repair/src/main.rs
--- day1-report-repair/target/debug/deps/day1_report_repair-930d8c3e20a4c2cb.d
+++
@@ -1,5 +0,0 @@
-/home/rafaavc/Documents/projects/advent-of-code-2020/day1-report-repair/target/debug/deps/day1_report_repair-930d8c3e20a4c2cb: src/main.rs
-
-/home/rafaavc/Documents/projects/advent-of-code-2020/day1-report-repair/target/debug/deps/day1_report_repair-930d8c3e20a4c2cb.d: src/main.rs
-
-src/main.rs:
