3c703d66cbdecf139882f71e30b2052fb364cb98
rafaavc <rafavc2508@gmail.com>
2021-12-01 14:57:47 +0000

    Day 1 solved with fold

--- 2021/src/main/kotlin/Day01.kt
+++ 2021/src/main/kotlin/Day01.kt
@@ -3,25 +3,19 @@ import utils.FileManager
 fun main() {
     val fileManager = FileManager(1)
     val lines = fileManager.readPuzzle1()
-
-    val numbers = mutableListOf(0)
-    numbers.addAll(lines.map { line -> line.toInt() })
-
-    val firstNumbersIdx = 1 // because we added the 0 for accumulation
+    val numbers = lines.map { line -> line.toInt() }
 
     fun solvePuzzle1() {
-        val count = numbers.reduceIndexed { idx, acc, el ->
-            if (idx > firstNumbersIdx && el > numbers[idx - 1])
-                acc + 1
+        val count = numbers.foldIndexed(0) { idx, acc, el ->
+            if (idx > 0 && el > numbers[idx - 1]) acc + 1
             else acc
         }
         println(count)
     }
 
     fun solvePuzzle2() {
-        val count = numbers.reduceIndexed { idx, acc, el ->
-            if (idx > firstNumbersIdx + 2 && el > numbers[idx - 3])
-                acc + 1
+        val count = numbers.foldIndexed(0) { idx, acc, el ->
+            if (idx > 2 && el > numbers[idx - 3]) acc + 1
             else acc
         }
         println(count)
