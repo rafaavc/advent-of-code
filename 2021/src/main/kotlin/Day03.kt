b66910df2bc3a699ac896d33caa648444959e76a
rafaavc <rafavc2508@gmail.com>
2021-12-04 18:45:38 +0000

    Days 3 & 4 done

---
+++ 2021/src/main/kotlin/Day03.kt
@@ -0,0 +1,70 @@
+import utils.FileManager
+import utils.Logger
+import kotlin.math.abs
+import kotlin.math.pow
+
+enum class CommonBitType {
+    MOST,
+    LEAST
+}
+
+fun main() {
+    val lines = FileManager.read(3)
+
+    fun get1CountAtI(lines: List<String>, i: Int): Int = lines.fold(0) { acc, line ->
+        if (line[i] == '1') acc + 1
+        else acc
+    }
+
+    fun getMostLeastCommonBitAtI(lines: List<String>, i: Int, type: CommonBitType): Char {
+        val oneCount = get1CountAtI(lines, i)
+        return when (type) {
+            CommonBitType.MOST -> if (oneCount >= lines.size - oneCount) '1' else '0'
+            CommonBitType.LEAST -> if (oneCount >= lines.size - oneCount) '0' else '1'
+        }
+    }
+
+    fun binaryToInt(binary: String): Int {
+        var result = 0
+        for (idx in binary.length - 1 downTo 0) {
+            if (binary[idx] == '0') continue
+            val value = 2.0.pow(abs(idx.toDouble() - binary.length + 1)).toInt()
+            result += value
+        }
+
+        return result
+    }
+
+    fun solvePuzzle1(): Int {
+        var gamma = 0
+        var epsilon = 0
+        val lineLength = lines[0].length - 1
+
+        for (idx in lineLength downTo 0) {
+            val value = 2.0.pow(abs(idx.toDouble() - lineLength)).toInt()
+
+            if (getMostLeastCommonBitAtI(lines, idx, CommonBitType.MOST) == '1') gamma += value
+            else epsilon += value
+        }
+
+        return gamma * epsilon
+    }
+
+    fun solvePuzzle2(): Int {
+        var oxygenLines = lines
+        var scrubberLines = lines
+        var count = 0
+
+        while (oxygenLines.size != 1 && scrubberLines.size != 1) {
+            val mostCommon = getMostLeastCommonBitAtI(oxygenLines, count, CommonBitType.MOST)
+            val leastCommon = getMostLeastCommonBitAtI(scrubberLines, count, CommonBitType.LEAST)
+            oxygenLines = oxygenLines.filter { it[count] == mostCommon }
+            scrubberLines = scrubberLines.filter { it[count] == leastCommon }
+            count++
+        }
+
+        return binaryToInt(scrubberLines[0]) * binaryToInt(oxygenLines[0])
+    }
+
+    Logger.logResults(solvePuzzle1(), solvePuzzle2())
+}
---
+++ 2021/src/main/kotlin/Day04.kt
@@ -0,0 +1,86 @@
+import utils.FileManager
+import utils.Logger
+
+class Puzzle(board: Array<Array<Int>>) {
+    private val numbersIndexes: MutableMap<Int, Pair<Int, Int>> = mutableMapOf()
+    private val indexesNumbers: MutableMap<Pair<Int, Int>, Int> = mutableMapOf()
+    var isOver = false
+        private set
+
+    init {
+        for ((y, line) in board.withIndex()) {
+            for ((x, number) in line.withIndex()) {
+                numbersIndexes[number] = Pair(x, y)
+                indexesNumbers[Pair(x, y)] = number
+            }
+        }
+    }
+
+    fun setNumber(number: Int): Boolean {
+        if (isOver) return false
+        val position = numbersIndexes[number] ?: return false
+
+        numbersIndexes.remove(number)
+        indexesNumbers[position] = -1
+
+        var horizontal = true
+        var vertical = true
+
+        for (i in 0..4) {
+            if (indexesNumbers[Pair(i, position.second)]!! >= 0) horizontal = false
+            if (indexesNumbers[Pair(position.first, i)]!! >= 0) vertical = false
+        }
+
+        isOver = horizontal || vertical
+        return isOver
+    }
+
+    fun getUnsetSum(): Int
+        = numbersIndexes.keys.fold(0) { acc, number ->
+            if (number >= 0) acc + number
+            else acc
+        }
+}
+
+fun main() {
+    val lines = FileManager.read(4)
+    val numbers = lines[0].split(",").map { line -> line.toInt() }
+    val boards: MutableList<Array<Array<Int>>> = mutableListOf()
+
+    var linePointer = 0
+    for (i in 1 until lines.size) {
+        if (lines[i] == "") {
+            boards.add(Array(5) { Array(5) { 0 } })
+            linePointer = 0
+            continue
+        }
+
+        boards.last()[linePointer] = lines[i]
+            .split(" ")
+            .filter { line -> line != "" }
+            .map { line -> line.toInt() }
+            .toTypedArray()
+        linePointer++
+    }
+
+    val puzzles = boards.map { board -> Puzzle(board) }
+
+    fun solvePuzzle1(): Int {
+        for (number in numbers)
+            for (puzzle in puzzles)
+                if (puzzle.setNumber(number))
+                    return number * puzzle.getUnsetSum()
+        return -1
+    }
+
+    fun solvePuzzle2(): Int {
+        var last = -1
+        for (number in numbers)
+            for (puzzle in puzzles)
+                if (puzzle.setNumber(number))
+                    last = number * puzzle.getUnsetSum()
+        return last
+    }
+
+    Logger.logResults(solvePuzzle1(), solvePuzzle2())
+}
