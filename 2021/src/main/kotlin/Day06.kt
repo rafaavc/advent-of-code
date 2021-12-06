03bb48c25f48dd03e8d747df13db2738fe60ad1e
rafaavc <rafavc2508@gmail.com>
2021-12-06 16:04:33 +0000

    Added day 6

---
+++ 2021/src/main/kotlin/Day06.kt
@@ -0,0 +1,31 @@
+import utils.FileManager
+import utils.Logger
+
+fun main() {
+    val lines = FileManager.read(6)
+    val numbers: MutableList<Int> = lines[0].split(",").map { n -> n.toInt() }.toMutableList()
+
+    fun solve(days: Int): Long {
+        var memo = Array(9) { 0L }
+        for (number in numbers) memo[number] = memo[number] + 1
+
+        for (day in 1..days) {
+            val nextMemo = Array(9) { 0L }
+            for (n in memo.indices) {
+                if (n == 0) {
+                    nextMemo[8] = memo[n]
+                    nextMemo[6] = memo[n]
+                } else nextMemo[n - 1] = nextMemo[n - 1] + memo[n]
+            }
+            memo = nextMemo
+        }
+
+        return memo.fold(0L) { acc, value -> acc + value }
+    }
+
+    fun solvePuzzle1(): Long = solve(80)
+
+    fun solvePuzzle2(): Long = solve(256)
+
+    Logger.logResults(solvePuzzle1(), solvePuzzle2())
+}
--- 2021/src/main/kotlin/utils/Logger.kt
+++ 2021/src/main/kotlin/utils/Logger.kt
@@ -2,11 +2,11 @@ package utils
 
 class Logger {
     companion object {
-        private fun logResult(puzzle: Int, result: Int) {
+        private fun logResult(puzzle: Int, result: Any) {
             println("Puzzle $puzzle: $result")
         }
 
-        fun logResults(puzzle1: Int, puzzle2: Int) {
+        fun logResults(puzzle1: Any, puzzle2: Any) {
             logResult(1, puzzle1)
             logResult(2, puzzle2)
         }
